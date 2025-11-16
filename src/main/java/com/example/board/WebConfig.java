051dce8f93afa217658d1e6151be8ca978bb3c0e
difficcd <144074124+difficcd@users.noreply.github.com>
2025-11-16 15:33:31 +0900

    Update WebConfig.java

--- src/main/java/com/example/board/WebConfig.java
+++ src/main/java/com/example/board/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://devtest-theta-six.vercel.app", "http://localhost:3000") // Vercel, localhost 허용
+                .allowedOrigins(
+                        "https://devtest-theta-six.vercel.app",
+                        "https://devtest-aobrmjyzd-oweles-projects-e30770f6.vercel.app",
+                        "http://localhost:3000") // Vercel, localhost 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더를 허용
                 .allowCredentials(true) // 자격 증명 허용
